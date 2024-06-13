46fe70beb34859e57f7ff9f48c129fcc3b0880a6
sky <sky64redstone@gmail.com>
2024-06-13 12:13:36 +0200

    LINUX: Finished list.hpp, added sth in color.hpp, created image.hpp

---
+++ cmake-build-debug/CMakeFiles/Library.dir/main.cpp.o.ddi.d
@@ -0,0 +1,42 @@
+CMakeFiles/Library.dir/main.cpp.o.ddi: \
+ /home/sky/projects/Library/main.cpp /usr/include/stdc-predef.h \
+ /home/sky/projects/Library/lib/iostream.hpp \
+ /home/sky/projects/Library/lib/types.hpp \
+ /home/sky/projects/Library/lib/string.hpp \
+ /home/sky/projects/Library/lib/memory.hpp \
+ /home/sky/projects/Library/lib/math.hpp /usr/include/unistd.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/timesize.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/bits/posix_opt.h /usr/include/bits/environments.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/bits/time64.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/14.1.1/include/stddef.h \
+ /usr/include/bits/confname.h /usr/include/bits/getopt_posix.h \
+ /usr/include/bits/getopt_core.h /usr/include/bits/unistd_ext.h \
+ /usr/include/linux/close_range.h \
+ /home/sky/projects/Library/lib/array.hpp \
+ /home/sky/projects/Library/lib/atomic.hpp \
+ /home/sky/projects/Library/lib/bit.hpp \
+ /home/sky/projects/Library/lib/color.hpp \
+ /home/sky/projects/Library/lib/list.hpp \
+ /home/sky/projects/Library/lib/iostream.hpp \
+ /home/sky/projects/Library/lib/mat.hpp \
+ /home/sky/projects/Library/lib/vec.hpp \
+ /home/sky/projects/Library/lib/math.hpp \
+ /home/sky/projects/Library/lib/memory.hpp \
+ /home/sky/projects/Library/lib/string.hpp \
+ /home/sky/projects/Library/lib/time.hpp /usr/include/time.h \
+ /usr/include/bits/time.h /usr/include/bits/timex.h \
+ /usr/include/bits/types/struct_timeval.h \
+ /usr/include/bits/types/clock_t.h /usr/include/bits/types/time_t.h \
+ /usr/include/bits/types/struct_tm.h \
+ /usr/include/bits/types/struct_timespec.h /usr/include/bits/endian.h \
+ /usr/include/bits/endianness.h /usr/include/bits/types/clockid_t.h \
+ /usr/include/bits/types/timer_t.h \
+ /usr/include/bits/types/struct_itimerspec.h \
+ /usr/include/bits/types/locale_t.h /usr/include/bits/types/__locale_t.h \
+ /home/sky/projects/Library/lib/types.hpp \
+ /home/sky/projects/Library/lib/vec.hpp \
+ /home/sky/projects/Library/lib/vector.hpp
