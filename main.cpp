03ee276760f66396bcd997c9a772083b5ddf9ef9
Sky64Redstone <sky64redstone@gmail.com>
2024-07-25 18:58:06 +0200

    WIN32: main.cpp: test program with std::thread :(

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "lib/iostream.hpp"
 #include "lib/array.hpp"
+#include "lib/assert.hpp"
 #include "lib/atomic.hpp"
 #include "lib/bit.hpp"
 #include "lib/color.hpp"
@@ -15,19 +16,34 @@
 #include "lib/vector.hpp"
 #include "lib/window.hpp"
 
+#include <thread>
+
 #define unused(x) (void)(x)
 
-int main() {
-    lib::window win{};
-    win.create({ 100, 100 }, { 250, 250 });
+lib::window win{};
 
-    unused(win.set_title("test"));
-
-    win.start_system_event_loop();
+void update() {
+    lib::cout << "entering update loop!\n";
 
     while (win.open()) {
         win.handle_system_events();
     }
+
+    win.cleanup();
+}
+
+int main() {
+    win.create({ 100, 100 }, { 250, 250 });
+
+    lib::cout << "created!\n";
+
+    std::thread thread{update};
+
+    lib::window::start_system_event_loop();
+
+    lib::cout << "waiting for thread!\n";
+
+    thread.join();
 
     return 0;
 }
